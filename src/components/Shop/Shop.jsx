a3c60bd9d24da4855d632cf4bde90ec2caa24c76
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-03-28 13:23:41 +0600

    fix some problem

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 import Product from '../product/Product';
 import './Shop.css'
@@ -8,27 +8,50 @@ const Shop = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res=> res.json())
-        .then(data=> setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
 
-    const handleAddToCart = product =>{
+    const handleAddToCart = product => {
         const newCart = [...cart, product];
         setCart(newCart)
 
         addToDb(product.id)
     }
 
+    useEffect(() => {
+        const storedCart = getShoppingCart()
+        const saveCart = [];
+        //step 1: get id
+        for (const id in storedCart) {
+            //step 2: get the product by using id 
+            const addedProduct = products.find(product => product.id === id);
+
+            //3: get quantity of the product 
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+
+                //4: add the added product  to the saved cart
+                saveCart.push(addedProduct)
+            }
+
+        }
+        //5: set  cart
+        setCart(saveCart)
+
+    }, [products])
+
     return (
         <div className='shop-container'>
             <div className='product-container'>
                 {
-                    products.map(product=> <Product 
-                    key={product.id}
-                    product={product}
-                    handleAddToCart={handleAddToCart}
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
@@ -36,7 +59,7 @@ const Shop = () => {
             <div className='cart-container-shop'>
                 <Cart cart={cart}></Cart>
             </div>
-            
+
         </div>
     );
 };
